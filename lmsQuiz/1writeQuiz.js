8ef3b6aab9455b44a0a6e42e32a7ee148af01592
john <kodchanat.viskul@hotmail.com>
2015-09-30 02:34:25 +0700

    add lms quiz bot

---
+++ lmsQuiz/1writeQuiz.js
@@ -0,0 +1,79 @@
+javascript:(
+	function(){
+		var test = true;
+		var submit = true;
+		var splitText = "&$#,";
+		var indexUrl = document.URL.indexOf("id=");
+		/*if(indexUrl == -1) indexUrl = document.URL.indexOf("q=")-2;
+		else */indexUrl -= 3;
+		var urlId = document.URL.substr(indexUrl);
+		var subject = document.getElementById("content").getElementsByTagName("h2")[0].innerText.substr(0,10);
+		subject = removeSpace(subject);
+		if(test) console.log(subject);
+		var form = document.forms['responseform'];
+		var div_bucket = form.getElementsByTagName('div')[0];
+
+		for(var i = 0; i < div_bucket.children.length; i++){
+			var item = div_bucket.children[i]; /*each question*/
+			var head = item.getElementsByClassName('qtext')[0]; /*header of question*/
+			var number = item.getElementsByClassName('no')[0]; /*question number*/
+			var choices = item.getElementsByTagName('label');
+			
+			if(head && number){ 
+				number = number.innerText; /*to string*/
+				number = number.substr(number.indexOf(" ")); /*get only digit*/
+				number = Number(number); /*to number*/
+				var caption = head.getElementsByTagName('b')[0];
+				if(caption){
+					caption = caption.getElementsByTagName('span')[0];
+					caption = caption.innerText; /*describe of question*/
+				}
+
+				var old, textChoice = undefined;
+				if(caption) old = getCookie(caption);
+				else old = getCookie(number);
+
+				if(old.indexOf(splitText) == 0){ /*found correct one*/
+					old = old.substr(splitText.length); /*old is correct choice*/
+					for(var j = 0; j < choices.length && textChoice != old; j++){
+						textChoice = choices[j].lastChild.data.trim();
+					}
+				}
+				else{
+					old = old.split(splitText); /*old is array of not correct choices*/
+					for(var j = 0, k = 0; j < choices.length && k != old.length; j++){
+						textChoice = choices[j].lastChild.data.trim();
+						for(k = 0; k < old.length && old[k] != textChoice; k++);
+					}
+				}
+				if(test) console.log("select "+j+textChoice);
+				choices[j-1].control.checked = true;
+
+				if(test) console.log("No",number,caption," item:",item);
+
+			}
+		}
+		/*submit*/
+		if(submit) div_bucket.getElementsByClassName("submitbtns")[0].lastElementChild.click();
+		if(test) console.log("DONE");
+
+		function getCookie(cname) {
+			cname = removeSpace(cname);
+		    var name = subject + cname + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0; i<ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0)==' ') c = c.substring(1);
+		        if (c.indexOf(name) == 0) {
+		            return c.substring(name.length, c.length);
+		        }
+		    }
+		    return "";
+		}
+		function removeSpace(str){
+			if(typeof(str) != "string");
+			str = str.toString();
+			return str.replace(/\s/g,'');
+		}
+	}()
+);
---
+++ lmsQuiz/2checkCorrect.js
@@ -0,0 +1,85 @@
+javascript:(
+	function(){
+		var test = true;
+		var finish = true;
+		var cookie = true;
+		var splitText = "&$#,";
+		var indexUrl = document.URL.indexOf("id=");
+		/*if(indexUrl == -1) indexUrl = document.URL.indexOf("q=")-2;
+		else */indexUrl -= 3;
+		var urlId = document.URL.substr(indexUrl);
+		var subject = document.getElementById("content").getElementsByTagName("h2")[0].innerText.substr(0,10);
+		subject = removeSpace(subject);
+		if(test) console.log(subject);
+		var div_bucket = document.getElementsByClassName('que');
+
+		for(var i = 0; i < div_bucket.length; i++){
+			var item = div_bucket[i]; /*each question*/
+			var head = item.getElementsByClassName('qtext')[0]; /*header of question*/
+			var number = item.getElementsByClassName('no')[0]; /*question number*/
+			var choices = item.getElementsByTagName('label');
+			
+			if(head && number){ 
+				number = number.innerText; /*to string*/
+				number = number.substr(number.indexOf(" ")); /*get only digit*/
+				number = Number(number); /*to number*/
+				var caption = head.getElementsByTagName('b')[0];
+				if(caption){
+					caption = caption.getElementsByTagName('span')[0];
+					caption = caption.innerText; /*describe of question*/
+				}
+				var textChoice = undefined,check = undefined;
+				for(var j = 0; j < choices.length && check == undefined; j++){
+					textChoice = choices[j].innerText;
+					textChoice = textChoice.substr(textChoice.indexOf(".")+1).trim();
+					check = choices[j].getElementsByTagName('img')[0];
+					if(check) check = check.alt;
+					if(0) console.log(textChoice, check);
+				}
+
+				if(test) console.log("No",number,caption," item:",item);
+				if(test) console.log("selected "+textChoice+ " : "+check);
+
+				var keyMsg;
+				if(caption) keyMsg = caption;
+				else keyMsg = number;
+
+				if(cookie && check == "Incorrect") addCookie(keyMsg, textChoice);
+				else if(cookie && check == "Correct") setCookie(keyMsg, splitText+textChoice);
+			}
+		}
+		/*finish*/
+		if(finish) document.forms[0].submit();
+		if(test) console.log("DONE");
+
+		function getCookie(cname) {
+			cname = removeSpace(cname);
+		    var name = subject + cname + "=";
+		    var ca = document.cookie.split(';');
+		    for(var i=0; i<ca.length; i++) {
+		        var c = ca[i];
+		        while (c.charAt(0)==' ') c = c.substring(1);
+		        console.log(c);
+		        if (c.indexOf(name) == 0) {
+		            return c.substring(name.length, c.length);
+		        }
+		    }
+		    return "";
+		}
+		function setCookie(cname,cvalue) {
+			cname = removeSpace(cname);
+		    document.cookie = subject + cname+"="+cvalue;
+		    console.log("seted"+subject + cname+"="+cvalue+";");
+		}
+		function addCookie(cname, cvalue){
+			var old = getCookie(cname);
+			if(old != "") cvalue = old + splitText + cvalue;
+			setCookie(cname, cvalue);
+		}
+		function removeSpace(str){
+			if(typeof(str) != "string");
+			str = str.toString();
+			return str.replace(/\s/g,'');
+		}
+	}()
+);
